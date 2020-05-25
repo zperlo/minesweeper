c8feff9db4e6fde9d7a8a56f328841d174ac2f83
zperlo <zip5@case.edu>
2020-05-24 19:05:10 -0700

    initial commit

---
+++ src/Game.java
@@ -0,0 +1,24 @@
+public class Game {
+    /*
+    Constraints:
+
+    NxN grid with B bombs
+    All cells in grid show number of bombs adjacent
+    Needs a user able to choose a square to expose
+    If bomb hit, game lose, if all squares but bombs hit, game win
+    If number hit, number shown, if blank hit, all connected blanks and numbers adjacent to those blanks are revealed
+    User can flag bombs
+     */
+
+    /*
+    Requirements:
+
+    Grid class
+    Game class
+    Cell class
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
