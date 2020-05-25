9e3ac07df54d39859d8a84b7de2e7ad48937e3a4
zperlo <zip5@case.edu>
2020-05-24 19:39:42 -0700

    implement grid initialization

--- src/Grid.java
+++ src/Grid.java
@@ -1,2 +1,60 @@
+import java.util.Random;
+
 public class Grid {
+
+    private Cell[][] grid;
+
+    private int numBombs;
+
+    public Grid(int size, int numBombs){
+        grid = new Cell[size][size];
+
+        // initialize grid cells
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[i].length; j++){
+                grid[i][j] = new Cell();
+            }
+        }
+
+        this.numBombs = numBombs;
+
+        // create bombs
+        while(numBombs > 0){
+            Random rand = new Random();
+            int row = rand.nextInt(size);
+            int col = rand.nextInt(size);
+            if(grid[row][col].getType() != Cell.CellType.BOMB){
+                grid[row][col].setType(Cell.CellType.BOMB);
+                incrementSurroundingBombCounts(row, col);
+                numBombs--;
+            }
+        }
+
+        // mark other cells as blank or numbered
+        for(Cell[] cellArr : grid){
+            for(Cell cell : cellArr){
+                if(cell.getType() != Cell.CellType.BOMB){
+                    if(cell.getNumber() == 0){
+                        cell.setType(Cell.CellType.BLANK);
+                    } else{
+                        cell.setType(Cell.CellType.NUMBER);
+                    }
+                }
+            }
+        }
+    }
+
+    // increases the bomb count of all cells surrounding a cell with coordinates row,col
+    private void incrementSurroundingBombCounts(int row, int col){
+        int[][] surroundings = new int[][]{{-1,-1}, {-1,0}, {-1,1}, {0,1}, {1,1}, {1,0}, {1,-1}, {0,-1}};
+        for(int[] surrounding : surroundings){
+            int nextRow = row + surrounding[0];
+            int nextCol = col + surrounding[1];
+            if(nextRow >= 0 && nextRow < grid.length){
+                if (nextCol >= 0 && nextCol < grid[nextRow].length){
+                    grid[nextRow][nextCol].incrementNumber();
+                }
+            }
+        }
+    }
 }
